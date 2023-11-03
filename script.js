95b25fabc0bcd97662b142ee5c7c28fca75ed091
Subhajit Mandal <108950928+Subhajit-mn@users.noreply.github.com>
2023-11-03 09:25:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 function decimalToBinary(num) {
   //Write you code here
-  let output = "";
-	while(num > 0){
-		if(num % 2 == 1){
-			output = "1" + output;
-		}else{
-			output = "0" + output;
-		}
-		num = Math.floor(num / 2);
-	}
-	//console.log(output);
+  const output = num.toString(2);
+	return output;
 }
 
 window.decimalToBinary = decimalToBinary;
